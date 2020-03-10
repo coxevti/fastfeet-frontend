d1042cfe419f7f47bedda07ae182b63c97c10d4c
Valdir Coxev <valcox.russo@gmail.com>
2020-03-10 14:49:38 -0400

    Adding configuration route private

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,5 +1,7 @@
 import React from 'react';
-import { Switch, Route } from 'react-router-dom';
+import { Switch } from 'react-router-dom';
+
+import Route from '~/routes/Route';
 
 import SignIn from '~/pages/SignIn';
 
@@ -9,7 +11,7 @@ export default function Routes() {
   return (
     <Switch>
       <Route path="/" exact component={SignIn} />
-      <Route path="/dashboard" component={Dashboard} />
+      <Route path="/dashboard" component={Dashboard} isPrivate />
     </Switch>
   );
 }
