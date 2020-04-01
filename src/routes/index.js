a6842ebc2e777244a4d8560bc14c95b5103a59ef
Valdir Coxev [Job] <valcox.russo@gmail.com>
2020-04-01 16:27:47 -0400

    Adding route deliverers, recipients and problems

--- src/routes/index.js
+++ src/routes/index.js
@@ -4,14 +4,19 @@ import { Switch } from 'react-router-dom';
 import Route from '~/routes/Route';
 
 import SignIn from '~/pages/SignIn';
-
 import Order from '~/pages/Order';
+import Delivery from '~/pages/Delivery';
+import Recipient from '~/pages/Recipient';
+import Problem from '~/pages/Problem';
 
 export default function Routes() {
   return (
     <Switch>
       <Route path="/" exact component={SignIn} />
       <Route path="/orders" component={Order} isPrivate />
+      <Route path="/deliverers" component={Delivery} isPrivate />
+      <Route path="/recipients" component={Recipient} isPrivate />
+      <Route path="/problems" component={Problem} isPrivate />
     </Switch>
   );
 }
