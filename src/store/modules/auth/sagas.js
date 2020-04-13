0279d81a014957e1bce210d2987a1d9b160cf595
Valdir Coxev [Job] <valdircoxevdev@gmail.com>
2020-04-13 16:28:17 -0400

    Adding sagas auth

--- src/store/modules/auth/sagas.js
+++ src/store/modules/auth/sagas.js
@@ -1,3 +1,24 @@
-import { all } from 'redux-saga/effects';
+import { all, takeLatest, put, call } from 'redux-saga/effects';
+import cogoToast from 'cogo-toast';
 
-export default all([]);
+import history from '~/services/history';
+import api from '~/services/api';
+import { signInSuccess } from './actions';
+
+function* signIn({ payload }) {
+  try {
+    const { email, password } = payload;
+    const response = yield call(api.post, '/sessions', {
+      email,
+      password,
+    });
+    const { token, user } = response.data;
+    yield put(signInSuccess(token, user));
+    history.push('/orders');
+    cogoToast.success(`Bem-vindo de volta ${user.name} :)`);
+  } catch (error) {
+    cogoToast.error(error.response.data.message);
+  }
+}
+
+export default all([takeLatest('@auth/SIGN_IN_REQUEST', signIn)]);
